67042c3cfab38ff4865662ceb8f77e58fd3e6a99
YagoPeixoto <yagopeixoto19@gmail.com>
2017-02-13 00:21:13 -0200

    ex poo

--- PooAula1-NotaAluno/src/br/com/yago/Main.java
+++ PooAula1-NotaAluno/src/br/com/yago/Main.java
@@ -10,34 +10,36 @@ public class Main {
 		
 		aluno alu = new aluno();
 		
-		entrada = new Scanner(System.in);
-        String nome;
-        
+		entrada = new Scanner(System.in);// lê a entrada do user
+		
         System.out.println("Digite seu nome: ");
-        nome = entrada.toString();
-        alu.nome = nome;
+        alu.nome = entrada.nextLine(); //chama o metodo scanner para que o user digite algo e add a entrada fornecida pelo user no atributo "nome" do abjeto aluno
 		
-		//alu.nome = "jao";
-		alu.diciplina = "matematica";
-		alu.nota1 = 8.2;
-		alu.nota2 = 6.0;
-		alu.nota3 = 8.4;
-		alu.nota4 = 7.0;
+        System.out.println("Digite a diciplina: ");
+        alu.diciplina = entrada.nextLine();
+        
+        System.out.println("Digite a nota 1: ");
+        alu.nota1 = entrada.nextDouble();
+        
+        System.out.println("Digite a nota 2: ");
+        alu.nota2 = entrada.nextDouble();
+        
+        System.out.println("Digite a nota 3: ");
+        alu.nota3 = entrada.nextDouble();
+        
+        System.out.println("Digite a nota 4: ");
+        alu.nota4 = entrada.nextDouble();
 		
+        
 		double media = (alu.nota1 + alu.nota2 + alu.nota3 + alu.nota4) / 4;
 		
-		System.out.println("A media do aluno é:"+ media);
+		System.out.println("O aluno "+ alu.nome + " na diciplina de "+ alu.diciplina + " tem a média de:"+ media + "\nSeu status na diciplina é:");
 		
 		if (media >= 7.0){
 			System.out.println("Aluno aprovado");
 		}else{
 			System.out.println("Aluno Reprovado");
 		}
-		
-		
-		
-		
-	
 
 	}
 
