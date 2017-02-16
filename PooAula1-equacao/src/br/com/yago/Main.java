88fc5b5bc869d025ee879f9f04d2886b96b558d0
YagoPeixoto <yagopeixoto19@gmail.com>
2017-02-16 20:43:05 -0200

    ex poo

--- PooAula1-equacao/src/br/com/yago/Main.java
+++ PooAula1-equacao/src/br/com/yago/Main.java
@@ -9,8 +9,21 @@ public class Main {
 		equacao.b = 6;
 		equacao.c = 8;
 		
+		double delta, x1, x2;
 		
+		delta = Math.pow(equacao.a,2) - 4 * equacao.a * equacao.b;
 
+		if (delta > 0){
+			
+			System.out.println("Não existe raiz");
+			
+		}else{
+			x1 =(- equacao.b + Math.sqrt(delta)/(2* equacao.a));
+			x2 =(- equacao.b - Math.sqrt(delta)/(2* equacao.a));
+			System.out.println("Delta: "+delta+ " x1 = "+ x1+" x2 = "+ x2);
+			
+		}
+		
 	}
 
 }
