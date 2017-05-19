85b7add3bda5d0f66a082770c162a49381fa63d6
Stênio O. Cruz <stenio22@gmail.com>
2017-05-19 13:48:16 -0300

    Update Main.java

--- Aula6Ex1/src/br/com/yago/Main.java
+++ Aula6Ex1/src/br/com/yago/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Funcionario.imprime(800, 800 + (800*0.1), "Yago", "Estudante");
+		Funcionario.imprime(800, 800 + (800*0.1), "pOmBo", "Engenheiro Master da informatica");
 		
 	}
 
